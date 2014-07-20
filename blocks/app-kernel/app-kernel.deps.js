588ba49de829be6f70236772e684aff67f9dbfca
Alexander Savin <box.for.postman@gmail.com>
2014-07-20 19:22:11 +0400

    Update bem-core dependency

--- blocks/app-kernel/app-kernel.deps.js
+++ blocks/app-kernel/app-kernel.deps.js
@@ -1,9 +1,9 @@
-({//todo: remove functions deps after https://github.com/bem/bem-core/issues/532 fix
+({
     mustDeps: [
         {block: 'i-bem', elems: ['dom']}
     ],
     shouldDeps: [
-        'functions', 'app-router-base', 'i-page', 'request-listener', 'app-api-requester', 'app-navigation',
+        'app-router-base', 'i-page', 'request-listener', 'app-api-requester', 'app-navigation',
         'vow', 'page', 'page-404', 'page-500', {block: 'app-kernel', mods: {'init-auto': true}}, 'app-logger'
     ]
 })
