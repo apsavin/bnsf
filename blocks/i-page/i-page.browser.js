3b3d300f86d656e8353114eaa8cc87642ef23e3c
Alexander Savin <box.for.postman@gmail.com>
2014-11-09 22:00:24 +0300

    Fix IPage#_replace browser method

--- blocks/i-page/i-page.browser.js
+++ blocks/i-page/i-page.browser.js
@@ -1,5 +1,7 @@
 /**@module i-page*/
-modules.define('i-page', ['i-bem__dom', 'BEMHTML', 'BEMTREE'], function (provide, BEMDOM, BEMHTML, BEMTREE, page) {
+modules.define('i-page', [
+    'i-bem__dom', 'BEMHTML', 'BEMTREE', 'app-api-requester'
+], function (provide, BEMDOM, BEMHTML, BEMTREE, appApiRequester, page) {
     "use strict";
 
     /**
@@ -12,11 +14,12 @@ modules.define('i-page', ['i-bem__dom', 'BEMHTML', 'BEMTREE'], function (provide
         /**
          * @param {String} blockName
          * @param {Object} BEMJSON
+         * @param {ApiRequester} [apiRequester]
          * @returns {vow:Promise}
          * @protected
          */
-        _replace: function (blockName, BEMJSON) {
-            return BEMTREE.apply(BEMJSON).then(function (BEMJSON) {
+        _replace: function (blockName, BEMJSON, apiRequester) {
+            return BEMTREE.apply(BEMJSON, apiRequester || appApiRequester).then(function (BEMJSON) {
                 IPage.replace(this.findBlockInside(blockName).domElem, BEMHTML.apply(BEMJSON));
             }, this);
         }
