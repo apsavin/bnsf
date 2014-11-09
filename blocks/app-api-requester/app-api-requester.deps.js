24342dbefbb5b1b52475f628964bc70ee238faa8
Alexander Savin <box.for.postman@gmail.com>
2014-11-09 22:45:23 +0300

    Remove app-api-requester node module

--- blocks/app-api-requester/app-api-requester.deps.js
+++ blocks/app-api-requester/app-api-requester.deps.js
@@ -1,3 +1,3 @@
 ({
-    shouldDeps: ['api-requester', 'app-router-base', 'app-api-router']
+    shouldDeps: ['api-requester', 'app-router-base']
 })
--- blocks/app-api-requester/app-api-requester.node.js
+++
@@ -1,9 +0,0 @@
-modules.define('app-api-requester', [
-    'app-api-router', 'api-requester'
-], function (provide, router, ApiRequester) {
-    "use strict";
-
-    provide(new ApiRequester(null, {
-        router: router
-    }));
-});
