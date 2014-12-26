76870af1c19088828ff4e5cef7b3087a8e37c732
Alexander Savin <box.for.postman@gmail.com>
2014-12-26 21:57:58 +0300

    Fix app-kernel deps

--- blocks/app-kernel/app-kernel.deps.js
+++ blocks/app-kernel/app-kernel.deps.js
@@ -5,6 +5,6 @@
     shouldDeps: [
         'app-router-base', 'i-page', 'request-listener', 'app-api-requester', 'app-navigation',
         'page', 'page-404', 'page-403', 'page-500', {block: 'app-kernel', mods: {'init-auto': true}, elems: ['config']},
-        'app-logger', 'app-api-router', 'api-requester', 'objects'
+        'app-logger', 'app-api-router', 'api-requester', 'objects', 'vow'
     ]
 })
