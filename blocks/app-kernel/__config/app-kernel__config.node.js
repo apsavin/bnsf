18db1496b9f3243678264b8a8bd8c919d1ae89f6
jifeon <balakirev.andrey@gmail.com>
2015-03-09 12:58:00 +0300

    Fix node servers config settings

--- blocks/app-kernel/__config/app-kernel__config.node.js
+++ blocks/app-kernel/__config/app-kernel__config.node.js
@@ -1,9 +1,8 @@
 modules.define('app-kernel__config', ['objects'], function (provide, objects, config) {
     "use strict";
 
-    provide(objects.extend(config, {
+    provide(objects.extend({
         port: 3000,
         staticHost: 'localhost:8080'
-    }));
-
+    }, config));
 });
